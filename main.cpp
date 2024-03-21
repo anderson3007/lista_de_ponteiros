81df7e0b6e9833cdf3de62f585fe15affcecad7e
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2024-03-21 14:03:40 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+  int i=3,j=5;
+  int *p, *q;
+  bool result;
+  p = &i;
+  q = &j;
+  
+  result = p == &j;
+  printf("%d\n", result);
+
+  // O resultado é 0, pois o ponteiro p aponta para o endereço de i e o ponteiro q aponta para o endereço de j. Ou seja, p e q não apontam para o mesmo endereço de memória.
+  
+}
