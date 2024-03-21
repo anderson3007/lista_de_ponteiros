c7fcf81ea888b1e4c7aa8401b98d75bd47499333
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2024-03-21 14:05:14 -0300

    Update questao01.cpp

--- questao01.cpp
+++ questao01.cpp
@@ -9,6 +9,8 @@ int main(void) {
   result = p == &j;
   printf("%d\n", result);
 
-  // O resultado é 0, pois o ponteiro p aponta para o endereço de i e o ponteiro q aponta para o endereço de j. Ou seja, p e q não apontam para o mesmo endereço de memória.
+  //O resultado é 0, pois o ponteiro p aponta para o endereço
+    de i e o ponteiro q aponta para o endereço de j. 
+    Ou seja, p e q não apontam para o mesmo endereço de memória.
   
 }
